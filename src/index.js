a3d2519c44bdd34424f6c3f988d7807c688da3f4
tetedacier <fabien.jacq@gmail.com>
2020-04-20 13:05:09 +0200

    Reject error instead of throwing

--- src/index.js
+++ src/index.js
@@ -58,7 +58,7 @@ const walkThroughDir = ({ dir, algorithm, resolve, reject, files } = {  }) => {
 const listClientChar = (dir, algorithm) => new Promise((resolve, reject) => {
     readdir(dir, (error, files) => {
         if(error)  {
-            throw error
+            reject(error)
         }
         walkThroughDir({ dir, algorithm, resolve, reject, files })
     })
