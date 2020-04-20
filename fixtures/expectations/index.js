0ba3d1aa126b6cb5cc51857eb1af796265d0cf86
tetedacier <fabien.jacq@gmail.com>
2020-04-20 11:01:44 +0200

    Add string API expectation

--- fixtures/expectations/index.js
+++ fixtures/expectations/index.js
@@ -3,5 +3,9 @@ module.exports = {
         'subfolder/subsubfolder/a.png': 'sha256-UsCeFZgF8JGsvAJ6Rdi/T1jRh5Yivv2MKhU+vRsBVlg=',
         'subfolder/a.css': 'sha256-WxSFMgSzxcznhC/Go8vPE4iV9WxKl5vXjDrcshjiqoQ=',
         'a.js': 'sha256-7Sg2OudLH+fRuNT0I4GWEadiNXe119hWkxqZR/W9DzQ='
+  },
+  stringFingerPrint:{
+      input: 'test',
+      output: 'sha256-n4bQgYhMfWWaL+qgxVrQFaO/TxsrC4Is0V1sFbDwCgg='
   }
-};
+}
