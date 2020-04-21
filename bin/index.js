7f89f46942f51a6680ff45fba0f5f6fc06b16ad3
tetedacier <fabien.jacq@gmail.com>
2020-04-21 09:19:35 +0200

    Enforce JSON output

--- bin/index.js
+++ bin/index.js
@@ -7,7 +7,7 @@ const {
 
 if (dirname) {
   getFilesFingerPrint(dirname)
-    .then(result => console.log(result))
+    .then(result => console.log(JSON.stringify(result, null, 2)))
     .catch(error => {
       console.error(error)
       process.exit(1)
